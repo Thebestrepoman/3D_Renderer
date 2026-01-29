f243aceeaab3828543dc5baa04ce28287e4be167
gsluzh <grigiriy.sluzhak@gmail.com>
2026-01-30 00:17:05 +0300

    dear parser

--- utility/object_parser.cpp
+++ utility/object_parser.cpp
@@ -8,6 +8,8 @@ std::vector<Triangle> ExtractFromObj(const std::string& filename) {
     std::vector<std::array<int, 3>> faces;
     std::string line;
     std::string cur;
+    Vec3 sum_points = Vec3::Zero();
+    int num_points = 0;
     while (std::getline(file, line)) {
         std::stringstream ss(line);
         ss >> cur;
@@ -15,6 +17,8 @@ std::vector<Triangle> ExtractFromObj(const std::string& filename) {
             double v1, v2, v3;
             ss >> v1 >> v2 >> v3;
             points.emplace_back(v1, v2, v3);
+            sum_points += points.back();
+            num_points++;
         }
         if (cur == "f") {
             int a, b, c;
@@ -22,9 +26,14 @@ std::vector<Triangle> ExtractFromObj(const std::string& filename) {
             faces.push_back({a, b, c});
         }
     }
+    sum_points /= num_points;
     std::vector<Vec3> normals(points.size(), Vec3::Zero());
     for (size_t i = 0; i < faces.size(); i++) {
-        Vec3 fnorm = CalculateNormalForThreeVectors(points[faces[i][0]], points[faces[i][1]], points[faces[i][2]]);
+        Vec3 fnorm = CalculateNormalForThreeVectors(points[faces[i][0] - 1], points[faces[i][1] - 1], points[faces[i][2] - 1]);
+        Vec3 center = (points[faces[i][0] - 1] + points[faces[i][1] - 1] + points[faces[i][2] - 1]) / 3;
+        if ((center - sum_points).dot(fnorm) < 0) {
+            fnorm = fnorm;
+        }
         normals[faces[i][0] - 1] += fnorm;
         normals[faces[i][1] - 1] += fnorm;
         normals[faces[i][2] - 1] += fnorm;

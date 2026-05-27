951f17d53f0de8d528f64e167e5f8935456f1a97
gsluzh <grigiriy.sluzhak@gmail.com>
2026-05-27 12:11:33 +0300

    include_fix

--- utility/object_parser.h
+++ utility/object_parser.h
@@ -2,10 +2,6 @@
 
 #include "triangle.h"
 
-#include <string>
-#include <fstream>
-#include <sstream>
-
 namespace renderer {
 std::vector<Triangle> ExtractFromObj(const std::string& filename);
 }  // namespace renderer
